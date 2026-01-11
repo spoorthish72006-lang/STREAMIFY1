0d21f302e2def853effefd2134f158787ca2db50
dusklight00 <r.rahul.developer@gmail.com>
2026-01-11 19:50:44 +0530

    code refactor

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 import "dotenv/config";
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
+import agentRoutes from './routes/agentRoutes.js';
+import metricsRoutes from './routes/metricsRoutes.js';
+import settingsRoutes from './routes/settingsRoutes.js';
+import ticketRoutes from './routes/ticketRoutes.js';
 import { connectDB } from './lib/db.js';
 
 const app = express();
@@ -10,7 +14,11 @@ const PORT = process.env.PORT || 5001;
 app.use(express.json());
 
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/agents", agentRoutes);
+app.use("/api/metrics", metricsRoutes);
+app.use("/api/settings", settingsRoutes);
+app.use("/api/tickets", ticketRoutes);
 app.listen(PORT,() => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
